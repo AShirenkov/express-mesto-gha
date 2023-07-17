c3b76f6f0b37e58865531dddf23f61a8ae453177
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-07-17 20:04:27 +0300

    fix: step3. перенести проверку ошибок юзеров в отдельный файл

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
-const User = require("../models/user");
+const User = require('../models/user');
 const {
   checkMongoId,
   throwErrorResponse,
   checkObject,
-} = require("./validation");
+  checkAvatarRequest,
+  checkProfileRequest,
+} = require('./validation');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -37,19 +39,21 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateProfile = (req, res) => {
   // дополнительная проверка чтобы не переписали этим запросом аватар
-  if (req.body.avatar || !(req.body.name && req.body.about)) {
-    return res.status(400).send({ message: "Ошибка валидации" });
-  }
-  User.findByIdAndUpdate(
-    req.user._id,
-    req.body,
-    // Передадим объект опций:
-    {
-      new: true, // обработчик then получит на вход обновлённую запись
-      runValidators: true, // данные будут валидированы перед изменением
-      upsert: false, // если пользователь не найден, он не будет создан
-    }
-  )
+  // if (req.body.avatar || !(req.body.name && req.body.about)) {
+  //   return res.status(400).send({ message: "Ошибка валидации" });
+  // }
+  checkProfileRequest(req)
+    .then(() => User.findByIdAndUpdate(
+      req.user._id,
+      req.body,
+      // Передадим объект опций:
+      {
+        new: true, // обработчик then получит на вход обновлённую запись
+        runValidators: true, // данные будут валидированы перед изменением
+        upsert: false, // если пользователь не найден, он не будет создан
+      },
+    ))
+
     .then((user) => {
       checkObject(user, res);
     })
@@ -58,21 +62,23 @@ module.exports.updateProfile = (req, res) => {
 
 module.exports.updateAvatar = (req, res) => {
   // console.log(req.body);
-  console.log(req.body.name);
+  // console.log(req.body.name);
   // дополнительная проверка чтобы не переписали этим запросом имя и описание
-  if (!req.body.avatar || req.body.name || req.body.about) {
-    return res.status(400).send({ message: "Ошибка валидации" });
-  }
-  User.findByIdAndUpdate(
-    req.user._id,
-    req.body,
-    // Передадим объект опций:
-    {
-      new: true, // обработчик then получит на вход обновлённую запись
-      runValidators: true, // данные будут валидированы перед изменением
-      upsert: false, // если пользователь не найден, он не будет создан
-    }
-  )
+  // if (!req.body.avatar || req.body.name || req.body.about) {
+  //   return res.status(400).send({ message: "Ошибка валидации" });
+  // }
+  checkAvatarRequest(req)
+    .then(() => User.findByIdAndUpdate(
+      req.user._id,
+      req.body,
+      // Передадим объект опций:
+      {
+        new: true, // обработчик then получит на вход обновлённую запись
+        runValidators: true, // данные будут валидированы перед изменением
+        upsert: false, // если пользователь не найден, он не будет создан
+      },
+    ))
+
     .then((user) => {
       checkObject(user, res);
     })
--- controllers/validation.js
+++ controllers/validation.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const checkMongoId = (id) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     const err = new Error();
-    err.name = 'ValidationIdError';
+    err.name = "ValidationIdError";
     return Promise.reject(err);
 
     // return res.status(400).send({ message: "Ошибка валидации" });
@@ -14,34 +14,63 @@ const checkMongoId = (id) => {
 const checkObject = (obj, res) => {
   if (!obj) {
     const err = new Error();
-    err.name = 'RequestError';
+    err.name = "RequestError";
     return Promise.reject(err);
   }
 
   return res.status(200).send(obj);
 };
 
+const checkAvatarRequest = (req) => {
+  if (req.body.avatar || !(req.body.name && req.body.about)) {
+    const err = new Error();
+    err.name = "ValidationAvatarError";
+    return Promise.reject(err);
+  }
+  return Promise.resolve();
+};
+const checkProfileRequest = (req) => {
+  if (!req.body.avatar || req.body.name || req.body.about) {
+    const err = new Error();
+    err.name = "ValidationProfileError";
+    return Promise.reject(err);
+  }
+  return Promise.resolve();
+};
+
 const throwErrorResponse = (err, res) => {
-  if (err.name === 'ValidationIdError') {
+  if (err.name === "ValidationIdError") {
     return res.status(400).send({
-      message: 'Переданы некорректные данные для запроса. Неверный ID',
+      message: "Переданы некорректные данные для запроса. Неверный ID",
     });
   }
-  if (err.name === 'ValidationError') {
+  if (err.name === "ValidationError") {
     return res.status(400).send({
-      message: 'Ошибка валидации данных',
+      message: "Ошибка валидации данных",
     });
   }
-  if (err.name === 'RequestError') {
+  if (err.name === "ValidationProfileError") {
+    return res.status(400).send({
+      message: "Некореректный зароос на обновление профиля",
+    });
+  }
+  if (err.name === "ValidationAvatarError") {
+    return res.status(400).send({
+      message: "Некореректный зароос на обновление аватара",
+    });
+  }
+  if (err.name === "RequestError") {
     return res.status(404).send({
-      message: 'Запрашиваемые данные отсутствуют',
+      message: "Запрашиваемые данные отсутствуют",
     });
   }
-  return res.status(500).send({ message: 'Произошла ошибка' });
+  return res.status(500).send({ message: "Произошла ошибка" });
 };
 // err.name = "RequestError";
 module.exports = {
   checkMongoId,
   throwErrorResponse,
   checkObject,
+  checkAvatarRequest,
+  checkProfileRequest,
 };
