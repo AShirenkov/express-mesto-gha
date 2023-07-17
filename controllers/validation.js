f31968d2b0be487ce64f904bb449825f14e096da
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-07-17 17:18:11 +0300

    feat: этап 2 по вынесению в отдельный файл

--- controllers/validation.js
+++ controllers/validation.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const checkMongoId = (id) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     const err = new Error();
-    err.name = 'ValidationIdError';
+    err.name = "ValidationIdError";
     return Promise.reject(err);
 
     // return res.status(400).send({ message: "Ошибка валидации" });
@@ -11,16 +11,37 @@ const checkMongoId = (id) => {
   return Promise.resolve();
 };
 
+const checkObject = (obj, res) => {
+  if (!obj) {
+    const err = new Error();
+    err.name = "RequestError";
+    return Promise.reject(err);
+  }
+
+  return res.status(200).send(obj);
+};
+
 const throwErrorResponse = (err, res) => {
-  if (err.name === 'ValidationIdError') {
+  if (err.name === "ValidationIdError") {
     return res.status(400).send({
-      message: 'Переданы некорректные данные для запроса. Неверный ID',
+      message: "Переданы некорректные данные для запроса. Неверный ID",
     });
   }
-  return res.status(500).send({ message: 'Произошла ошибка' });
+  if (err.name === "ValidationError") {
+    return res.status(400).send({
+      message: "Ошибка валидации данных",
+    });
+  }
+  if (err.name === "RequestError") {
+    return res.status(404).send({
+      message: "Запрашиваемые данные отсутствуют",
+    });
+  }
+  return res.status(500).send({ message: "Произошла ошибка" });
 };
 // err.name = "RequestError";
 module.exports = {
   checkMongoId,
   throwErrorResponse,
+  checkObject,
 };
