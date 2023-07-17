e85980b7bb548e2249f0f927abb90b10586f7e52
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-07-17 15:02:17 +0300

    feat: добавлена обработка ошибок карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,42 +1,83 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     // возвращаем записанные в базу данные пользователю
-    .then((cards) => res.send(cards))
+    .then((cards) => {
+      if (!cards) {
+        return res
+          .status(404)
+          .send({ message: "Запрашиваемые данные отсутствуют" });
+      }
+      res.status(200).send(cards);
+    })
+
     // если данные не записались, вернём ошибку
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка карты 1' }));
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
 module.exports.createCard = (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user })
-    .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка карты 2' }));
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
+      }
+
+      res.status(500).send({ message: "Произошла ошибка" });
+    });
 };
 
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка карты 3' }));
+    .then((card) => {
+      if (!card) {
+        return res
+          .status(404)
+          .send({ message: "Запрашиваемые данные отсутствуют" });
+      }
+      res.send(card);
+    })
+
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true },
+    { new: true }
   )
-    .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка карты 3' }));
+    .then((card) => {
+      if (!card) {
+        return res
+          .status(404)
+          .send({ message: "Запрашиваемые данные отсутствуют" });
+      }
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
+      }
+
+      res.status(500).send({ message: "Произошла ошибка" });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
-    { new: true },
+    { new: true }
   )
-    .then((card) => res.send(card))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка карты 3' }));
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: "Ошибка валидации" });
+      }
+
+      res.status(500).send({ message: "Произошла ошибка" });
+    });
 };
