d0b7e453832a828c4d90da4725a541078c07250d
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-07-17 15:50:22 +0300

    fix: проверка на несуществующтий адрес

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-const express = require('express');
+const express = require("express");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // Слушаем 3000 порт
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const routerUsers = require('./routes/users'); // импортируем роутер
+const routerUsers = require("./routes/users"); // импортируем роутер
 
-const routerCards = require('./routes/cards'); // импортируем роутер
+const routerCards = require("./routes/cards"); // импортируем роутер
 
 const { PORT = 3000 } = process.env;
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {});
 
 const app = express();
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 app.use((req, res, next) => {
   req.user = {
-    _id: '64b3d42e3c656c07033d984a',
+    _id: "64b3d42e3c656c07033d984a",
   };
 
   next();
 });
 
-app.use('/users', routerUsers); // запускаем
-app.use('/cards', routerCards); // запускаем
+app.use("/users", routerUsers); // запускаем
+app.use("/cards", routerCards); // запускаем
+app.use("/", (req, res, next) => {
+  res.status(404).send({ message: "такого адреса не существует" });
+  next();
+});
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
