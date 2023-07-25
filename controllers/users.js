b4dc962693a37c975508db5fbfb89f3ef2ea3a5a
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-07-25 20:44:15 +0300

    feat: Добавить в схему пользователя и пароль. Захешировать пароль

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,13 @@
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 const {
   checkMongoId,
   throwErrorResponse,
   checkObject,
   checkAvatarRequest,
   checkProfileRequest,
-} = require('./validation');
-const { statusCode } = require('../utils/constants');
+} = require("./validation");
+const { statusCode } = require("../utils/constants");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -23,7 +24,17 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  User.create(req.body)
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) =>
+      User.create({
+        email: req.body.email,
+        password: hash,
+        avatar: req.body.avatar,
+        name: req.body.name,
+        about: req.body.about,
+      })
+    )
     .then((user) => res.status(statusCode.created).send(user))
     .catch((err) => throwErrorResponse(err, res));
 };
@@ -32,16 +43,18 @@ module.exports.updateProfile = (req, res) => {
   // дополнительная проверка чтобы не переписали этим запросом аватар
 
   checkProfileRequest(req)
-    .then(() => User.findByIdAndUpdate(
-      req.user._id,
-      req.body,
-      // Передадим объект опций:
-      {
-        new: true, // обработчик then получит на вход обновлённую запись
-        runValidators: true, // данные будут валидированы перед изменением
-        upsert: false, // если пользователь не найден, он не будет создан
-      },
-    ))
+    .then(() =>
+      User.findByIdAndUpdate(
+        req.user._id,
+        req.body,
+        // Передадим объект опций:
+        {
+          new: true, // обработчик then получит на вход обновлённую запись
+          runValidators: true, // данные будут валидированы перед изменением
+          upsert: false, // если пользователь не найден, он не будет создан
+        }
+      )
+    )
 
     .then((user) => checkObject(user, res))
     .catch((err) => throwErrorResponse(err, res));
@@ -51,16 +64,18 @@ module.exports.updateAvatar = (req, res) => {
   // дополнительная проверка чтобы не переписали этим запросом имя и описание
 
   checkAvatarRequest(req)
-    .then(() => User.findByIdAndUpdate(
-      req.user._id,
-      req.body,
-      // Передадим объект опций:
-      {
-        new: true, // обработчик then получит на вход обновлённую запись
-        runValidators: true, // данные будут валидированы перед изменением
-        upsert: false, // если пользователь не найден, он не будет создан
-      },
-    ))
+    .then(() =>
+      User.findByIdAndUpdate(
+        req.user._id,
+        req.body,
+        // Передадим объект опций:
+        {
+          new: true, // обработчик then получит на вход обновлённую запись
+          runValidators: true, // данные будут валидированы перед изменением
+          upsert: false, // если пользователь не найден, он не будет создан
+        }
+      )
+    )
 
     .then((user) => checkObject(user, res))
     .catch((err) => throwErrorResponse(err, res));
--- models/user.js
+++ models/user.js
@@ -1,25 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const isEmail = require("validator/lib/isEmail");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    // имя пользователя, строка от 2 до 30 символов, обязательное поле;
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: "Неправильный формат почты",
+    },
+  },
+  password: {
     type: String,
     required: true,
     minlength: 2,
+  },
+  name: {
+    // имя пользователя, строка от 2 до 30 символов, обязательное поле;
+    type: String,
+
+    minlength: 2,
     maxlength: 30,
+    default: "Жак-Ив Кусто",
   },
   about: {
     // информация о пользователе, строка от 2 до 30 символов, обязательное поле;
     type: String,
-    required: true,
+
     minlength: 2,
     maxlength: 30,
+    default: "Исследователь",
   },
   avatar: {
     // ссылка на аватарку, строка, обязательное поле.
     type: String,
-    required: true,
+
+    default:
+      "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
   },
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
