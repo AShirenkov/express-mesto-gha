c1a3e9095a5354a23a7a8cb448bc701cd778ba42
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-07-31 20:40:21 +0300

    feat: добавить классы ошибок

--- app.js
+++ app.js
@@ -41,6 +41,15 @@ app.use("/", (req, res, next) => {
     .send({ message: "такого адреса не существует" });
   next();
 });
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    // проверяем статус и выставляем сообщение в зависимости от него
+    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+  });
+});
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
