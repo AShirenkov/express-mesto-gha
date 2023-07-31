e219c44198fcd03a1aac27a83ef557cf6e21dc26
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-07-31 19:52:48 +0300

    fix: 8. Удалить хардкод

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const express = require('express');
+const express = require("express");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // Слушаем 3000 порт
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const routerUsers = require('./routes/users'); // импортируем роутер
+const routerUsers = require("./routes/users"); // импортируем роутер
 
-const routerCards = require('./routes/cards'); // импортируем роутер
+const routerCards = require("./routes/cards"); // импортируем роутер
 
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
 
-const { statusCode } = require('./utils/constants');
+const { statusCode } = require("./utils/constants");
 
 const { PORT = 3000 } = process.env;
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {});
 
 const app = express();
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64b3d42e3c656c07033d984a',
-  };
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: "64b3d42e3c656c07033d984a",
+//   };
 
-  next();
-});
+//   next();
+// });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post("/signin", login);
+app.post("/signup", createUser);
 app.use(auth);
 
-app.use('/users', routerUsers); // запускаем
-app.use('/cards', routerCards); // запускаем
-app.use('/', (req, res, next) => {
+app.use("/users", routerUsers); // запускаем
+app.use("/cards", routerCards); // запускаем
+app.use("/", (req, res, next) => {
   res
     .status(statusCode.notFound)
-    .send({ message: 'такого адреса не существует' });
+    .send({ message: "такого адреса не существует" });
   next();
 });
 
