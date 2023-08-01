2de568cb6829f79b81257ae5a1a2b21ef27b75f0
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-08-01 16:19:11 +0300

    fix. потерял требование к обязательности линка

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -53,6 +53,6 @@ module.exports.checkCard = celebrate({
     name: Joi.string().required().min(2).max(30),
     // link: Joi.string().required().min(18), // сделать проверку регуляркой на ссылку
 
-    link: Joi.string().regex(regexUrl),
+    link: Joi.string().required().regex(regexUrl),
   }),
 });
