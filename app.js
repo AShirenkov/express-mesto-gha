222fc57df1fd4ed95eb40d222ee510894a5c0c6a
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-08-02 12:21:54 +0300

    fix: вынести в отдельный мидлвар маршруты

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const { errors } = require('celebrate');
-const routerUsers = require('./routes/users'); // импортируем роутер
+// const routerUsers = require("./routes/users"); // импортируем роутер
 
-const routerCards = require('./routes/cards'); // импортируем роутер
+// const routerCards = require("./routes/cards"); // импортируем роутер
 
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+// const { createUser, login } = require("./controllers/users");
+// const auth = require("./middlewares/auth");
+const route = require('./middlewares/route');
 
-const NotFoundError = require('./errors/not-found-error');
-const { checkSignin, checkSignup } = require('./middlewares/requestValidation');
+// const NotFoundError = require("./errors/not-found-error");
+// const { checkSignin, checkSignup } = require("./middlewares/requestValidation");
 const handlerErrors = require('./middlewares/handlerErrors');
 
 const { PORT = 3000 } = process.env;
@@ -24,7 +25,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 const app = express();
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
-
+/*
 app.post('/signin', checkSignin, login);
 app.post('/signup', checkSignup, createUser);
 app.use(auth);
@@ -33,7 +34,8 @@ app.use('/users', routerUsers); // запускаем
 app.use('/cards', routerCards); // запускаем
 app.use('/', (req, res, next) => {
   next(new NotFoundError('Nакого адреса не существует'));
-});
+}); */
+app.use(route);
 app.use(errors());
 app.use(handlerErrors);
 
