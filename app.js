04a6f716546dc74cf6134d3d7e2699e41aea0648
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-08-04 23:31:37 +0300

    feat: добавлен логгер

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { errors } = require('celebrate');
 const route = require('./middlewares/route');
 
 const handlerErrors = require('./middlewares/handlerErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -18,8 +19,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 const app = express();
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+app.use(requestLogger); // подключаем логгер запросов
 
 app.use(route);
+app.use(errorLogger);
 app.use(errors());
 app.use(handlerErrors);
 
